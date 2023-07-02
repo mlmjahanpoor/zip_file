a0f9d7903c47d7a10ac941053d01e65bde1176d2
3510324447 <3510324447@PC-1177>
2023-07-02 11:08:16 +0330

    Add project files.

---
+++ SharpZip/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
+
+namespace SharpZip.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+    public HomeController(IWebHostEnvironment environment)
+    {
+            _environment = environment;
+    }
+    [HttpGet]
+    public IActionResult ZipDirectory()
+    {
+        var destination = Path.Combine(_environment.WebRootPath, "myZip.zip");
+        ZipFileMethods.ZipContentsOfDirectory(@"D:\test", destination);
+
+        return Ok();
+    }
+    
+    [HttpGet]
+    public IActionResult UnZipDirectory()
+    {
+        var source = Path.Combine(_environment.WebRootPath, "other");
+        ZipFileMethods.UnzipContentsOfDirectory(source,Path.Combine(_environment.WebRootPath,"myZip.zip"));
+
+        return Ok();
+    }
+}
